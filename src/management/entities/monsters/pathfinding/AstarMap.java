d873c0e20d021f7797128150675eb97884101394
darkxell <darkxell.mc@gmail.com>
2021-09-23 19:11:20 +0200

    Implemented Astar
    does NOT work. It's hard, okay?

--- src/management/entities/monsters/pathfinding/AstarMap.java
+++ src/management/entities/monsters/pathfinding/AstarMap.java
@@ -40,7 +40,7 @@ public class AstarMap {
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 	}
 
 	/** Return the array1d point in a 2d map. */
@@ -51,31 +51,36 @@ public class AstarMap {
 	public void print(Graphics2D g2d) {
 		for (int i = 0; i < this.width; i++)
 			for (int j = 0; j < this.height; j++)
-				try {
-					if (i == startX && j == startY)
-						g2d.setColor(new Color(0, 0, 255, 100));
-					else if (i == endX && j == endY)
-						g2d.setColor(new Color(255, 0, 0, 100));
-					else 
-						g2d.setColor(new Color(140, 140, 140, 100));
-					if (nodes[d1(i, j)].walkable) {
-						g2d.fillRect(16 * i + x + 2, 16 * j + y + 2, 12, 12);
-						g2d.setColor(Color.GRAY);
-						g2d.drawString("" + nodes[d1(i, j)].costF(), 16 * i + x + 3, 16 * j + y + 12);	
+				if (i > startX - 7 && i < startX + 7 && j > startY - 7 && j < startY + 7)
+					try {
+						if (i == startX && j == startY)
+							g2d.setColor(new Color(0, 0, 255, 100));
+						else if (i == endX && j == endY)
+							g2d.setColor(new Color(255, 0, 0, 100));
+						else
+							g2d.setColor(new Color(140, 140, 140, 100));
+						if (nodes[d1(i, j)].walkable) {
+							g2d.fillRect(16 * i + x + 2, 16 * j + y + 2, 12, 12);
+							g2d.setColor(Color.GRAY);
+							g2d.drawString("" + (nodes[d1(i, j)].costF() < 0 ? "?" : nodes[d1(i, j)].costF()),
+									16 * i + x + 3, 16 * j + y + 12);
+						}
+					} catch (Exception e) {
+						System.err
+								.println("Astar not printed in the current \"room\" at coordinates : " + i + " / " + j);
 					}
-				} catch (Exception e) {
-					System.err.println("Astar not printed in the current \"room\" at coordinates : " + i + " / " + j);
-				}
 	}
 
+	/** https://www.youtube.com/watch?v=-L-WgKMFuhE */
 	private void compute() {
-		System.out.println(" > Starting the astar compute!");
 		ArrayList<AstarTile> open = new ArrayList<>(20);
 		open.add(nodes[d1(startX, startY)]);
 		nodes[d1(startX, startY)].costG = 0;
 		nodes[d1(startX, startY)].costH = computeHcost(startX, startY, endX, endY);
-		
+
 		for (int maxiter = 0; maxiter < 200; maxiter++) {
+			if (open.size() == 0)
+				return;
 			AstarTile current = getCurrent(open);
 			open.remove(current);
 			current.closed = true;
@@ -87,13 +92,18 @@ public class AstarMap {
 
 			populateNeighbors(current);
 			for (int i = 0; i < neighbors.length; i++) {
-				
-				
-				
-				
-				
+				if (neighbors[i] == null || !neighbors[i].walkable || neighbors[i].closed)
+					continue;
+				int l = (neighbors[i].x == current.x || neighbors[i].y == current.y) ? 10 : 14;
+				if (current.costG + l < neighbors[i].costG || open.contains(neighbors[i])) {
+					AstarTile n = neighbors[i];
+					n.costG = current.x == n.x || current.y == n.y ? current.costG + 10 : current.costG + 14;
+					n.costH = computeHcost(n.x, n.y, endX, endY);
+					n.from = d1(current.x, current.y);
+					if (!open.contains(n))
+						open.add(n);
+				}
 			}
-			
 		}
 	}
 
@@ -107,23 +117,32 @@ public class AstarMap {
 	}
 
 	private int computeHcost(int x, int y, int destX, int destY) {
-		return 0;
+		int moveX = destX - x, moveY = destY - y;
+		if (moveX == moveY)
+			return 14 * moveX;
+		int ldiff = Math.abs(moveY - moveX);
+		int diag = Math.max(moveY, moveX) - ldiff;
+		return ldiff * 10 + diag * 14;
 	}
 
 	private AstarTile[] neighbors = new AstarTile[9];
 
-	/** Populates the neighbours aray with neighbours to a tile. Contents may be null if there's nothing there (treat as impassible terain).*/
+	/**
+	 * Populates the neighbours aray with neighbours to a tile. Contents may be null
+	 * if there's nothing there (treat as impassible terain).
+	 */
 	private void populateNeighbors(AstarTile source) {
-		neighbors[0] = source.x<=0?null:(source.y<=0?null:nodes[d1(source.x-1,source.y-1)]);
-		neighbors[1] = source.y<=0?null:nodes[d1(source.x,source.y-1)];
-		neighbors[2] = source.x>=width-1?null:(source.y<=0?null:nodes[d1(source.x+1,source.y-1)]);
-		
-		neighbors[3] = source.x<=0?null:nodes[d1(source.x-1,source.y)];
-		neighbors[4] = source.x>=width-1?null:nodes[d1(source.x+1,source.y)];
-		
-		neighbors[5] = source.x<=0?null:(source.y>=height-1?null:nodes[d1(source.x-1,source.y+1)]);
-		neighbors[6] = source.y>=height-1?null:nodes[d1(source.x,source.y+1)];
-		neighbors[7] = source.x>=width-1?null:(source.y>=height-1?null:nodes[d1(source.x+1,source.y+1)]);
+		neighbors[0] = source.x <= 0 ? null : (source.y <= 0 ? null : nodes[d1(source.x - 1, source.y - 1)]);
+		neighbors[1] = source.y <= 0 ? null : nodes[d1(source.x, source.y - 1)];
+		neighbors[2] = source.x >= width - 1 ? null : (source.y <= 0 ? null : nodes[d1(source.x + 1, source.y - 1)]);
+
+		neighbors[3] = source.x <= 0 ? null : nodes[d1(source.x - 1, source.y)];
+		neighbors[4] = source.x >= width - 1 ? null : nodes[d1(source.x + 1, source.y)];
+
+		neighbors[5] = source.x <= 0 ? null : (source.y >= height - 1 ? null : nodes[d1(source.x - 1, source.y + 1)]);
+		neighbors[6] = source.y >= height - 1 ? null : nodes[d1(source.x, source.y + 1)];
+		neighbors[7] = source.x >= width - 1 ? null
+				: (source.y >= height - 1 ? null : nodes[d1(source.x + 1, source.y + 1)]);
 	}
 
 }
